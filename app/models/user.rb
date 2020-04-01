7433c847d3038980d2bee985ddadcfa452246825
J Resilient <j.resilient.1@gmail.com>
2020-04-01 17:45:04 -0400

    Add users table and User model

---
+++ app/models/user.rb
@@ -0,0 +1,37 @@
+class User < ApplicationRecord
+    attr_reader :password
+    validates :username, :session_token, presence: true, uniqueness: true
+    validates :password_digest, presence: { message: 'Password can\'t be blank' }
+    validates :password, length: { minimum: 6, allow_nil: true }
+    after_initialize :ensure_session_token
+
+    def reset_session_token!
+        self.session_token = self.class.generate_session_token
+        self.save!
+        self.session_token
+    end
+
+    def self.generate_session_token
+        SecureRandom::urlsafe_base64(16)
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+        self.password_digest
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    def self.find_by_credentials(user_name, password)
+        user = User.find_by(username: user_name)
+        return user if user && user.is_password?(password)
+        nil
+    end
+
+    def ensure_session_token
+        self.session_token ||= self.class.generate_session_token
+    end
+end
