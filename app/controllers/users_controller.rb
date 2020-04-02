13d50427784dff0cac799a4f0aa1dc1f24e27744
J Resilient <j.resilient.1@gmail.com>
2020-04-02 15:59:16 -0400

    Fix typo in #user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,6 @@ class UsersController < ApplicationController
 
     private
     def user_params
-        params.require(:cat).permit(:username, :password)
+        params.require(:user).permit(:username, :password)
     end
 end
