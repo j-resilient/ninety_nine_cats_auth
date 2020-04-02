3be80babe5c71c562855c7cd31a788212bfb77b8
J Resilient <j.resilient.1@gmail.com>
2020-04-02 16:01:42 -0400

    Change #create to redirect to /cats on successful user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            render json: @user
+            redirect_to cats_url
         else
             flash.now[:errors] = @user.errors.full_messages
             render :new
