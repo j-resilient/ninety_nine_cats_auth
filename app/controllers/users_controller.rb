7516866b43c04edaad1b57ea1fd2301d7c04444a
J Resilient <j.resilient.1@gmail.com>
2020-04-02 15:50:04 -0400

    Add UsersController

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+    def new
+        @user = User.new
+        render :new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            render json: @user
+        else
+            flash.now[:errors] = @user.errors.full_messages
+            render :new
+        end
+    end
+
+    private
+    def user_params
+        params.require(:cat).permit(:username, :password)
+    end
+end
